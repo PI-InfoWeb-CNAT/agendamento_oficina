00c3ac8723e2d390a5e45daee4c592cd31bbb647
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-11-25 15:56:41 -0300

    entity

---
+++ oficina-do-marcio/Context/EFContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using oficina_do_marcio.Models;
+
+namespace oficina_do_marcio.Context
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("oficinadomarciodb") { }
+        public DbSet<Mecanico> Mecanicos { get; set; }
+    }
+}
---
+++ oficina-do-marcio/Models/Mecanico.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace oficina_do_marcio.Models
+{
+    public class Mecanico
+    {
+        [Key]
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
